2b43532f211e80b45fd30de71eb5068cadd59653
Grasiela Gomes <produtoradama@gmail.com>
2022-09-27 19:29:18 -0300

    refactor: add browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./themes/global";
 import { defaultTheme } from "./themes/default";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
