c5be75f6d308ce9d2132721917903b02d86ba390
Grasiela Gomes <produtoradama@gmail.com>
2022-10-27 20:10:41 -0300

    feat: add history helpers

---
+++ src/pages/history/History.helper.ts
@@ -0,0 +1,10 @@
+export const COLORS: { [key: string]: "yellow" | "red" | "green" } = {
+  started: "yellow",
+  stopped: "red",
+  finished: "green"
+};
+export const LABELS: { [key: string]: string } = {
+  started: "Em andamento",
+  stopped: "Interrompido",
+  finished: "Concluído"
+};
